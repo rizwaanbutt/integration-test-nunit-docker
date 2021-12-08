cb6951eb45ee919cb5d9071646e7024d0f504d56
rizwaanbutt <rbutt@calrom.com>
2021-12-08 15:20:42 +0000

    Docker support.

---
+++ src/IntegrationTestNunitDocker/Globit.OrderApi.UnitTests/IntegrationTests/OrdersTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Globit.OrderApi.Tests.IntegrationTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class OrdersTests : TestBase
+    {
+        [Test]
+        public async Task 
+    }
+}
